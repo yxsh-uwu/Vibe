c8de59ce7871fa1378541afd6b09d55c9430b7d7
Yash <yash27560@gmail.com>
2025-11-06 04:42:29 +0530

    TRPC Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import {prisma} from "@/lib/db";
+"use client"
+import { useTRPC } from "@/trpc/client";
+import { useQuery} from "@tanstack/react-query";
 
-const Page = ()=>{
+const Page = () => {
+  const trpc = useTRPC();
+  const {data}=useQuery(trpc.createAI.queryOptions({text:"Yash"}));
+  trpc.createAI.queryOptions({text:"Hello!"});
   
 
   return (
     <div>
+      {JSON.stringify(data)}
     </div>
   );
 }
